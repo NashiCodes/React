f7f931851337deff04e54ae7913b919d43812f29
João Pereira <nashi.codes@gmail.com>
2022-10-04 02:59:43 -0300

    aprendendo coisas basicas

--- minhapagina/src/App.js
+++ minhapagina/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const name = "Jão";
+  const upper = name.toUpperCase();
+
+  function soma(a, b) {
+    return a + b;
+  }
+
+  const url='https://via.placeholder.com/200'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MEU PROJETO HMTL EM REACT</h1>
+      <p>Olá, {upper}</p>
+      <p>Soma: {soma(4, 3)}</p>
+      <img src={url} alt=""/>
     </div>
   );
 }
